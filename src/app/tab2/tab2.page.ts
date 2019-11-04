e2cb317cd6a5263b9d2df8a3d84153c4f56ad8bc
Johan Nilsson <developerjohan01@gmail.com>
2019-11-04 13:41:43 +0200

    tab starter with iframe

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import {
+    Component,
+    ElementRef, OnInit,
+    ViewChild
+} from '@angular/core';
 
 @Component({
-  selector: 'app-tab2',
-  templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+    selector: 'app-tab2',
+    templateUrl: 'tab2.page.html',
+    styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+export class Tab2Page implements OnInit {
+    @ViewChild('iframeContainer', {static: true}) iframeContainer: ElementRef;
+    @ViewChild('iframeContainer2', {read: ElementRef, static: true}) iframeContainer2: ElementRef;
+    // @ViewChild('iframeContainer2', {static: false}) iframeContainer2: ElementRef;
 
-  constructor() {}
+    constructor() {
+        console.log('constructor');
+        console.log(this.iframeContainer);
+        console.log(this.iframeContainer2);
+    }
+
+    ngOnInit(): void {
+        console.log('ngOnInit');
+        console.log(this.iframeContainer);
+        console.log(this.iframeContainer2);
+    }
+
+    ionViewDidEnter() {
+        console.log('ionViewDidEnter');
+        console.log(this.iframeContainer);
+        console.log(this.iframeContainer2);
+    }
 
 }
